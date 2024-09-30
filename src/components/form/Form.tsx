f0b9442dbe6b78513ce0f7e10987344248e2f485
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-09-30 14:42:35 +0600

    create Form wrapper

---
+++ src/components/form/Form.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable padding-line-between-statements */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable prettier/prettier */
+'use client'
+import { ReactNode } from "react";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+
+interface formConfig {
+  defaultValues?: Record<string, any>;
+  resolver?: any;
+}
+
+interface IProps extends formConfig {
+  children: ReactNode;
+  onSubmit: SubmitHandler<any>;
+}
+
+const FXForm = ({ children, onSubmit, defaultValues, resolver }: IProps) => {
+  const formConfig: formConfig = {};
+
+  if (!!defaultValues) {
+    formConfig["defaultValues"] = defaultValues;
+  }
+  if (!!resolver) {
+    formConfig["resolver"] = resolver;
+  }
+
+  const methods = useForm(formConfig);
+
+  const submitHandler = methods.handleSubmit;
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={submitHandler(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
+};
+
+export default FXForm;
