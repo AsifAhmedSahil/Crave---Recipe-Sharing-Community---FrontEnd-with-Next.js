e38fc2cc7ff36984c02ee0100bcb6662d85694e3
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-09-30 14:33:29 +0600

    implement role-based routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,56 @@
-/* eslint-disable padding-line-between-statements */
+/* eslint-disable no-console */
+/* eslint-disable import/order */
 /* eslint-disable prettier/prettier */
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+
+
+const AuthRoutes = ['/login','/register']
+
+type Role = keyof typeof roleBasedRoutes
+
+const roleBasedRoutes = {
+    USER:[/^\/profile/],
+    ADMIN:[/^\/admin/],
+
+}
+
  
-const AuthRoutes = ["/login","/register"]
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-    const {pathname} = request.nextUrl
+export async function middleware(request: NextRequest) {
+
+    const {pathname} =request.nextUrl
+
 
     const user = {
-        name:"asif"
+        name:"asif",
+        role:"ADMIN"
     }
+    
 
-    // const user = undefined
+    
 
-   if(!user){
+    if(!user){
         if(AuthRoutes.includes(pathname)){
             return NextResponse.next()
-        } else{
-            return NextResponse.redirect(new URL('/login', request.url))
+        } else {
+            return NextResponse.redirect(new URL(`/login?redirect=${pathname}`, request.url))
         }
-   }
+    }
+
+    if(user?.role && roleBasedRoutes[user?.role as Role] ){
+        const routes = roleBasedRoutes[user?.role as Role]
+
+        if(routes.some((route) => pathname.match(route))){
+            return NextResponse.next()
+        }
+    }
+
 
   return NextResponse.redirect(new URL('/', request.url))
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/profile',"/login","/register"],
+  matcher: ['/login','/register','/profile',"/profile/:page*",'/admin'],
 }
