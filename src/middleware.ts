7477673f9981fa7f48b272feda31e46668fc1e41
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-09-30 12:39:16 +0600

    set user based middleware routing:

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,31 @@
+/* eslint-disable padding-line-between-statements */
 /* eslint-disable prettier/prettier */
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
+const AuthRoutes = ["/login","/register"]
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+    const {pathname} = request.nextUrl
 
     const user = {
         name:"asif"
     }
 
-    if(user.name){
-        return NextResponse.next()
-    }else{
-        return NextResponse.redirect(new URL('/login', request.url))
-    }
+    // const user = undefined
+
+   if(!user){
+        if(AuthRoutes.includes(pathname)){
+            return NextResponse.next()
+        } else{
+            return NextResponse.redirect(new URL('/login', request.url))
+        }
+   }
 
   return NextResponse.redirect(new URL('/', request.url))
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/profile'],
+  matcher: ['/profile',"/login","/register"],
 }
