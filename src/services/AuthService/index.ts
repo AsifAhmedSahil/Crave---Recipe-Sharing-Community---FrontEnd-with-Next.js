1614a56172ff876244152fba32ce921382c22551
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-10-01 17:44:10 +0600

    set tanstack query

--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -2,14 +2,20 @@
 "use server"
 
 import axiosInstance from "@/src/lib/AxiosInstance"
+import { cookies } from "next/headers"
 import { FieldValues } from "react-hook-form"
 
 export const registerUser = async (userData :FieldValues) =>{
     try {
 
-        const res = await axiosInstance.post("/auth/register",userData)
-        console.log(res.data)
-        return res.data
+        const {data} = await axiosInstance.post("/auth/register",userData)
+
+        if(data.success){
+            cookies().set("accessToken",data?.data?.accessToken) 
+            cookies().set("refreshToken",data?.data?.refreshToken) 
+        }
+        
+        return data
         
     } catch (error:any) {
         throw new Error(error)
